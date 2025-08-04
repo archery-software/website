9f68d0f9ccd53f80b1a25c6f238b8a9abc326288
fletchers <ath3ris@proton.me>
2025-08-04 16:08:04 +0100

    refactor: styling

--- src/app/sight-marks/page.tsx
+++ src/app/sight-marks/page.tsx
@@ -24,7 +24,7 @@ function interpolateMark(
 function SightMarks() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [inputParams, setInputParams] = React.useState({
-    arrow_speed_fps: 260,
+    arrow_speed_fps: 200,
     sight_mark_1: 40,
     sight_mark_2: 90,
     distance_1_yards: 20,
@@ -57,15 +57,15 @@ function SightMarks() {
 
   return (
     <div className="flex flex-col items-center gap-16 mx-auto mt-16 sm:flex-row justify-evenly">
-      <div>
+      <div className="flex flex-col gap-4">
         {calculations().map(([yards, mark]) => (
           <div key={yards}>
             {yards} yards: {mark}
           </div>
         ))}
       </div>
-      <div className="flex flex-col w-full max-w-sm gap-4 p-8 bg-white border rounded-lg shadow">
-        <label className="flex flex-col text-sm font-medium text-gray-700">
+      <div className="flex flex-col w-full max-w-sm gap-4 p-8 bg-white border rounded-lg shadow sm:max-w-md">
+        {/* <label className="flex flex-col text-sm font-medium text-gray-700">
           Arrow Speed (fps)
           <input
             type="number"
@@ -75,51 +75,55 @@ function SightMarks() {
             }
             className="px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
-        </label>
-        <label className="flex flex-col text-sm font-medium text-gray-700">
-          Sight Mark 1
-          <input
-            type="number"
-            value={inputParams.sight_mark_1}
-            onChange={(e) =>
-              setInputParams({ ...inputParams, sight_mark_1: Number(e.target.value) })
-            }
-            className="px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col text-sm font-medium text-gray-700">
-          Sight Mark 2
-          <input
-            type="number"
-            value={inputParams.sight_mark_2}
-            onChange={(e) =>
-              setInputParams({ ...inputParams, sight_mark_2: Number(e.target.value) })
-            }
-            className="px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col text-sm font-medium text-gray-700">
-          Distance 1 (yards)
-          <input
-            type="number"
-            value={inputParams.distance_1_yards}
-            onChange={(e) =>
-              setInputParams({ ...inputParams, distance_1_yards: Number(e.target.value) })
-            }
-            className="px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col text-sm font-medium text-gray-700">
-          Distance 2 (yards)
-          <input
-            type="number"
-            value={inputParams.distance_2_yards}
-            onChange={(e) =>
-              setInputParams({ ...inputParams, distance_2_yards: Number(e.target.value) })
-            }
-            className="px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </label>
+        </label> */}
+        <div className="flex gap-4">
+          <label className="flex flex-col text-xs font-medium text-gray-700 sm:text-sm">
+            Distance 1 (yards)
+            <input
+              type="number"
+              value={inputParams.distance_1_yards}
+              onChange={(e) =>
+                setInputParams({ ...inputParams, distance_1_yards: Number(e.target.value) })
+              }
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col text-xs font-medium text-gray-700 sm:text-sm">
+            Sight Mark 1
+            <input
+              type="number"
+              value={inputParams.sight_mark_1}
+              onChange={(e) =>
+                setInputParams({ ...inputParams, sight_mark_1: Number(e.target.value) })
+              }
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </label>
+        </div>
+        <div className="flex gap-4">
+          <label className="flex flex-col text-xs font-medium text-gray-700 sm:text-sm">
+            Distance 2 (yards)
+            <input
+              type="number"
+              value={inputParams.distance_2_yards}
+              onChange={(e) =>
+                setInputParams({ ...inputParams, distance_2_yards: Number(e.target.value) })
+              }
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col text-xs font-medium text-gray-700 sm:text-sm">
+            Sight Mark 2
+            <input
+              type="number"
+              value={inputParams.sight_mark_2}
+              onChange={(e) =>
+                setInputParams({ ...inputParams, sight_mark_2: Number(e.target.value) })
+              }
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </label>
+        </div>
       </div>
     </div>
   );
